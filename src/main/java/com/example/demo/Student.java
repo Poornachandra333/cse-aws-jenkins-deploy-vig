c95ede088c7c1835443e158e6790ccaebdbb5717
Poorna Chandra <poornarikki@gmail.com>
2026-04-05 14:43:37 +0530

    final

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Student {
 
+	@GetMapping("/")
 	public String studentDetails() {
 		return "Rohit sharma";
 	}
